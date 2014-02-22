467cd592babca4ffc3fb245ebdea4da914dffb43
carc11 <skwekamolfk@gmail.com>
2014-02-22 13:43:49 +0100

    combine

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-@users = User.all
-@hash = Gmaps4rails.build_markers(@users) do |user, marker|
-  marker.lat user.latitude
-  marker.lng user.longitude
-end
 end
