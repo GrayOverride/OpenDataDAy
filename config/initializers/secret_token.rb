316376143d1a8046fd569e3717264550386d4487
carc11 <skwekamolfk@gmail.com>
2014-02-22 10:38:52 +0100

    A new dawn

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OpenDataDay::Application.config.secret_key_base = '65c1df0804e39783103f934d82544e5a54109e73887c315653b2f38667985726623b691fef34ae2aac244e41e9d68bd9f7f691a813df67b85d7ba61e464d95bd'
